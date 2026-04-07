72440d773aa408c96a2a72f74c1499f488d17fd3
Ersilago1 <ekos2007@inbox.ru>
2026-04-07 21:44:31 +0500

    Lab10

--- Lab8/shop_back/api/urls.py
+++ Lab8/shop_back/api/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from . import views
 
-router = DefaultRouter()
-router.register(r'categories', views.CategoryViewSet, basename='category')
-router.register(r'products', views.ProductViewSet, basename='product')
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('products/', views.ProductListAPIView.as_view(), name='product-list'),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view(), name='product-detail'),
+    path('categories/', views.CategoryListAPIView.as_view(), name='category-list'),
+    path('categories/<int:category_id>/', views.CategoryDetailAPIView.as_view(), name='category-detail'),
+    path('categories/<int:category_id>/products/', views.CategoryProductsAPIView.as_view(), name='category-products'),
 ]
